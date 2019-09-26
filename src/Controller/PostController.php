b5d96634c618ec2781f29ea3cfb47f17ab9466b1
mzwae <mrmonjaie@gmail.com>
2019-09-26 22:42:00 +0100

    added post ctrl

---
+++ src/Controller/PostController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class PostController{
+  /**
+   * @Route("/")
+   */
+
+  public function index(){
+    return new Response('<html><body>Hello</body></html>');
+  }
+}
