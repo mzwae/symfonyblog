5124ea1b59f8ffcec5c18fb36381135ee99e4048
mzwae <mrmonjaie@gmail.com>
2019-09-28 22:42:04 +0100

    added navbar

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -4,14 +4,16 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class PostController{
+class PostController extends Controller{
   /**
    * @Route("/")
    * @Method({"GET"})
    */
 
   public function index(){
-    return new Response('<html><body>Hello</body></html>');
+    // return new Response('<html><body>Hello</body></html>');
+    return $this->render('posts/index.html.twig');
   }
 }
