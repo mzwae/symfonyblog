d3c9b86fd86f3ee5f39710c777d4b130ebb61752
mzwae <mrmonjaie@gmail.com>
2019-09-29 09:52:56 +0100

    created data migrations

---
+++ src/Entity/Post.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ */
+class Post
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+    * @ORM\Column(type="text", length=100)
+    */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $body;
+
+
+}
