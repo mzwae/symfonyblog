b4051346d0ee658a4e119991bf3a1eddd4248710
mzwae <mrmonjaie@gmail.com>
2019-10-04 23:46:49 +0100

    added new post feature

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -1,11 +1,18 @@
 <?php
 namespace App\Controller;
-
 use App\Entity\Post;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
 
 class PostController extends Controller{
   /**
@@ -30,6 +37,15 @@ class PostController extends Controller{
      $post = $this->getDoctrine()->getRepository(Post::class)->find($id);
      return $this->render('posts/show.html.twig', array('post' => $post));
    }
+
+   /**
+    * @Route("/post/new", name:"new_post")
+    * Method({"GET", "POST"})
+    */
+    public function new(){
+
+
+    }
 
   // /**
   //  * @Route("/post/save")
