acde7b34d1219adb7593de8b0b1bc1194678581a
mzwae <mrmonjaie@gmail.com>
2019-10-06 18:16:20 +0100

    added new post template

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -46,6 +46,17 @@ class PostController extends Controller{
           'attr' => array('class' => 'btn btn-primary mt-3')
         ))
         ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+          $article = $form->getData();
+          $entityManager = $this->getDoctrine()->getManager();
+          $entityManager->persist($article);
+          $entityManager->flush();
+
+          return $this->redirectToRoute('article_list');
+        }
       return $this->render('posts/new.html.twig', array(
         'form' => $form->createView()
       ));
